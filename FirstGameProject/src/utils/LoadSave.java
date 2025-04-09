daa536ae7475ce1400fc7dc3346bf8c721c7e723
teranuu <160133511+teranuu@users.noreply.github.com>
2025-04-09 12:02:07 +0800

    Update LoadSave.java

--- FirstGameProject/src/utils/LoadSave.java
+++ FirstGameProject/src/utils/LoadSave.java
@@ -15,6 +15,7 @@ public class LoadSave {
     public static final String LEVEL_ATLAS = "outdoor_sprite.png";
     public static final String LEVEL_ONE_DATA = "sea.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
+    public static final String MENU_BACKGROUND = "menu_background.png";
     
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
